c22aea2f998f76acde422bb44e64fb6cae9595d4
ahmed hesham <ahmedhesham9275@gmail.com>
2022-04-09 15:25:21 +0200

    final test

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-gmrnpe7s.us-east-1.elasticbeanstalk.com//api/v0'
+  apiHost: 'https://udagram-api-dev.eba-t6936c7e.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
